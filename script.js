f0c688c8e2fbffa5ca979e90786e9eec92596236
Tri Nanda <2001020024@student.umrah.ac.id>
2024-11-01 16:28:28 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDialog() {
+    document.getElementById("dialogOverlay").style.display = "block";
+    document.getElementById("dialogBox").style.display = "block";
+}
+
+function closeDialog() {
+    document.getElementById("dialogOverlay").style.display = "none";
+    document.getElementById("dialogBox").style.display = "none";
+}
